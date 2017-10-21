954ae3c9f6081d8692c9561a1537341f705a85d7
Nati Namvong <nati@flyingcomma.com>
2017-10-21 13:15:26 +0700

    add manu Squad Numbers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Nati from '@/components/Nati'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/nati',
+      name: 'Nati',
+      component: Nati
     }
   ]
 })
