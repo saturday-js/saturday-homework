1d4350a6c263ce4dc194709b6a9b58d5e64587fd
Nati Namvong <nati@flyingcomma.com>
2017-10-21 18:07:19 +0700

    add github button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-    <h1 class="title is-1">Saturday Homeworks.</h1>
+    <h1 class="title is-1">👩‍💻 👨‍💻 Saturday Homeworks.</h1>
+    <iframe src="https://ghbtns.com/github-btn.html?user=saturday-js&repo=saturday-homework&type=fork&count=true&size=large" frameborder="0" scrolling="0" width="158px" height="30px"></iframe>
+    <br><br>
     <table class="table is-striped is-hoverable is-fullwidth">
       <thead>
         <tr>
