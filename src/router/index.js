437102c11ba863f2215aea2237adab0cd486c1de
miw1513 <miw1513@hotmail.com>
2017-10-23 22:02:25 +0700

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Nati from '@/components/Nati'
-<<<<<<< HEAD
 import Rittiphon from '@/components/Rittiphon'
-=======
 import Rachata from '@/components/Rachata'
 
->>>>>>> bf281b1d3166dacb7e27d9458dc79c0c6442a363
 Vue.use(Router)
 
 export default new Router({
@@ -23,15 +20,14 @@ export default new Router({
       component: Nati
     },
     {
-<<<<<<< HEAD
       path: '/rittiphon',
       name: 'Rittiphon',
       component: Rittiphon
-=======
+    },
+    {
       path: '/Rachata',
       name: 'Rachata',
       component: Rachata
->>>>>>> bf281b1d3166dacb7e27d9458dc79c0c6442a363
     }
   ]
 })
