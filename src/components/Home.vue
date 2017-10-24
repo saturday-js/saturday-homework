9b5745fbd7862a0d58dc44cc17446ff5e0aeb5a9
yume045 <yume045@hotmail.com>
2017-10-25 02:46:40 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,6 +19,13 @@
             <router-link to="/nati">Manchester United 2017/18 Squad Numbers.</router-link>
           </td>
         </tr>
+        <tr>
+          <td>5806021630126</td>
+          <td>พีรพล สวัสดี</td>
+          <td>
+            <router-link to="/Fuse">World Time.</router-link>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
