bd9a92a362e43ab97945d5691a9539c6fb69ef3e
Nanthawad <5806021622123@fitm.kmutnb.ac.th>
2017-10-25 03:06:43 +0700

    All players FIFA World cup 2014

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Nati from '@/components/Nati'
+import nanthawad from '@/components/nanthawad'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/nati',
       name: 'Nati',
       component: Nati
+    },
+    {
+      path: '/nanthawad',
+      name: 'nanthawad',
+      component: nanthawad
     }
   ]
 })
