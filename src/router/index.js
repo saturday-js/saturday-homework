bf3a468147f888b7642bf699ce3de587ff506fa1
OilSirirat <32787859+OilSirirat@users.noreply.github.com>
2017-10-26 00:29:22 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Nati from '@/components/Nati'
-<<<<<<< HEAD
 import sirirat from '@/components/sirirat'
-
-=======
 import Sillawat from '@/components/Sillawat'
 import Rittiphon from '@/components/Rittiphon'
 import Rachata from '@/components/Rachata'
 import kawin from '@/components/kawin'
->>>>>>> 7447e89f4582290c04cae6d6ffc21adef71a303f
 Vue.use(Router)
 
 export default new Router({
@@ -26,11 +22,11 @@ export default new Router({
       component: Nati
     },
     {
-<<<<<<< HEAD
       path: '/sirirat',
       name: 'sirirat',
       component: sirirat
-=======
+    },
+    {
       path: '/Sillawat',
       name: 'Sillawat',
       component: Sillawat
@@ -49,7 +45,6 @@ export default new Router({
       path: '/kawin',
       name: 'kawin',
       component: kawin
->>>>>>> 7447e89f4582290c04cae6d6ffc21adef71a303f
     }
   ]
 })
