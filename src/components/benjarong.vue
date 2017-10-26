71e6e6b571e7d9199df9eec7237fbe2aead0ad42
benjarongruangsil <31164090+benjarongruangsil@users.noreply.github.com>
2017-10-26 23:47:18 +0700

    Update benjarong.vue

--- src/components/benjarong.vue
+++ src/components/benjarong.vue
@@ -1,49 +1,45 @@
 <template>
   <div class="container">
-
       <img src="https://dog.ceo/img/dog-api-logo.svg" height="10%" width="10%">
       <p class="title is-4">DOG</p>
+
   <center>
-         <!-- <select class="input primary outline"> -->
-            <button value="AFFENPINSCHER"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/affenpinscher/images')">AFFENPINSCHER</button>
-            <button value="AFRICAN"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/african/images')">AFRICAN</button>
-            <button value="AIREDALE"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/airedale/images')">AIREDALE</button>
-            <button value="AKITA"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/akita/images')">AKITA</button>
-            <button value="APPENZELLER"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/appenzeller/images')">APPENZELLER</button>
-            <button value="BASENJI" class="button is-small" @click="changeApi('https://dog.ceo/api/breed/basenji/images')">BASENJI</button>
-            <button value="BEAGLE"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/beagle/images')">BEAGLE</button>
-            <button value="BORZOI" class="button is-small" @click="changeApi('https://dog.ceo/api/breed/borzoi/images')">BORZOI</button>
-            <button value="BOUVIER"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/bouvier/images')">BOUVIER</button>
-            <button value="BOXER"  class="button is-small"@click="changeApi('https://dog.ceo/api/breed/boxer/images')">BOXER</button>
-         <!-- </select> -->
-
-      <div class="hero-body">
-        <img :src=dogs[showPic]  > </br></br>
-      </div>
-
-      <button type="button"@click="random()"class="button is-danger is-outlined">>More of this breed</button>
+    Show me a...  <select  v-model="getName"  class="button" aria-haspopup="true" aria-controls="dropdown-menu3" @click="changeApi()">
+            <option v-for="name in nameDog " >  {{  name }}  </option>
+      </select>
+  <div class="hero-body" v-for = "dog in dogs" >
+        <img :src= 'dog'  width="350px">
+  </div>
 
     </center>
   </div>
 </template>
-
 <script>
 export default {
   data () {
     return {
       loading: true,
       dogs: '',
-      api: 'https://dog.ceo/api/breed/affenpinscher/images',
-      showPic: 0
+      apiImage: '',
+      showPic: 0,
+      nameDog: '',
+      getName: ''
     }
   },
-  // created () {
-  //   changeApi('')
-  // },
+  created () {
+    this.changeApi()
+    this.api = 'https://dog.ceo/api/breeds/list/all'
+    fetch(this.api)
+    .then((res) => { return res.json() })
+    .then((res) => {
+      this.nameDog = Object.keys(res.message)
+      this.loading = false
+    })
+  },
   methods: {
-    changeApi: function (api) {
-      this.api = api
-      fetch(this.api)
+    changeApi: function () {
+      this.apiImage = 'https://dog.ceo/api/breed/' + this.getName + '/images'
+      fetch(this.apiImage)
       .then((res) => { return res.json() })
       .then((res) => {
         this.dogs = res.message
